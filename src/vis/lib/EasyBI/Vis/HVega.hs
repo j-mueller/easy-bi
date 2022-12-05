1f2ae24c22190ad85ac78c5ac58a60cd29b4d8e2
Jann Müller <j-mueller@users.noreply.github.com>
2022-12-05 17:11:42 +0100

    Add EasyBI.Vis.HVega

---
+++ src/vis/lib/EasyBI/Vis/HVega.hs
@@ -0,0 +1,124 @@
+{-# LANGUAGE DataKinds         #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE GADTs             #-}
+{-# LANGUAGE LambdaCase        #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE TypeOperators     #-}
+
+module EasyBI.Vis.HVega
+  ( vegaLite,
+
+    -- * Helper / utility
+    toHtml,
+    toHtmlFile,
+    toJSON,
+  )
+where
+
+import           Control.Lens           (_Just, to, (^.), (^..))
+import           Control.Monad.Writer   (MonadWriter, runWriter, runWriterT,
+                                         tell)
+import qualified Data.Aeson             as JSON
+import           Data.Foldable          (forM_, toList)
+import           Data.Sequence          (Seq)
+import qualified Data.Sequence          as Seq
+import qualified Data.Text.Lazy         as TL
+import           EasyBI.Vis.Types       (ColorChannel, Encoding, Mark (..),
+                                         MarkChannel, Measurement (..),
+                                         PositionChannel, Relation (..),
+                                         ScaleTp (..), colorChannel, field,
+                                         fieldName, mark, markChannel,
+                                         measurement, positionX, positionY,
+                                         scale, scaleTp, title)
+import           Graphics.Vega.VegaLite (BuildEncodingSpecs, Position (..),
+                                         PropertySpec, VegaLite)
+import qualified Graphics.Vega.VegaLite as VL
+
+{-| Convert an encoding to a 'VegaLite' specification
+-}
+vegaLite :: Relation f => Encoding f -> VegaLite
+vegaLite enc =
+  let (_, specs) = runWriter (writeSpecs enc)
+  in (VL.toVegaLite $ toList specs)
+
+toHtml :: Relation f => Encoding f -> TL.Text
+toHtml = VL.toHtml . vegaLite
+
+toHtmlFile :: (Relation f) => FilePath -> Encoding f -> IO ()
+toHtmlFile fp enc = VL.toHtmlFile fp (vegaLite enc)
+
+toJSON :: Relation f => Encoding f -> JSON.Value
+toJSON = VL.fromVL . vegaLite
+
+writeSpecs ::
+  forall f m.
+  ( Relation f
+  , MonadWriter (Seq PropertySpec) m
+  ) =>
+  Encoding f ->
+  m ()
+writeSpecs enc = do
+  (_, encoding) <- runWriterT $ do
+    writePositionChannel X (enc ^. positionX)
+    writePositionChannel Y (enc ^. positionY)
+    writeColorChannel (enc ^. colorChannel)
+  tell $ Seq.singleton $ VL.encoding (encoding [])
+  writeMarkChannel (enc ^. markChannel)
+
+writePositionChannel ::
+  forall f m.
+  (Relation f, MonadWriter BuildEncodingSpecs m) =>
+  Position ->
+  PositionChannel f ->
+  m ()
+writePositionChannel pos ch =
+  tell $ VL.position pos $
+    (ch ^.. field . _Just . to fieldName . to VL.PName)
+      <> (ch ^.. field . _Just . to measurement . to mkMeasurement . to VL.PmType)
+      <> (ch ^.. title . _Just . to VL.PTitle)
+      <> (ch ^.. scale . scaleTp . _Just . to (VL.PScale . mkScale))
+
+writeColorChannel ::
+  forall f m.
+  (Relation f, MonadWriter BuildEncodingSpecs m) =>
+  ColorChannel f ->
+  m ()
+writeColorChannel ch =
+  tell $ VL.color $
+    (ch ^.. field . _Just . to fieldName . to VL.MName)
+      <> (ch ^.. field . _Just . to (VL.MmType . mkMeasurement . measurement))
+
+writeMarkChannel ::
+  forall m.
+  (MonadWriter (Seq PropertySpec) m) =>
+  MarkChannel ->
+  m ()
+writeMarkChannel ch = forM_ (ch ^. mark) $ \mk -> do
+  tell $ Seq.singleton $ VL.mark (mkMark mk) []
+
+mkScale :: ScaleTp -> [VL.ScaleProperty]
+mkScale = \case
+  SLinear   -> [VL.SType VL.ScLinear]
+  SLog      -> [VL.SType VL.ScLog]
+  SPow      -> [VL.SType VL.ScPow]
+  STime     -> [VL.SType VL.ScTime]
+  SUtc      -> [VL.SType VL.ScUtc]
+  SQuantile -> [VL.SType VL.ScQuantile]
+  SOrdinal  -> [VL.SType VL.ScOrdinal]
+
+mkMeasurement :: Measurement -> VL.Measurement
+mkMeasurement = \case
+  Nominal      -> VL.Nominal
+  Ordinal      -> VL.Ordinal
+  Quantitative -> VL.Quantitative
+  TemporalRel  -> VL.Quantitative
+  TemporalAbs  -> VL.Temporal
+
+mkMark :: Mark -> VL.Mark
+mkMark = \case
+  Bar   -> VL.Bar
+  Point -> VL.Point
+  Line  -> VL.Line
+  Rect  -> VL.Rect
