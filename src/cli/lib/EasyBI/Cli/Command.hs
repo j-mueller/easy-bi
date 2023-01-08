cbce6b84545b154e9cd6c686479a3a58bcd40b90
Jann Müller <j-mueller@users.noreply.github.com>
2023-01-08 11:14:59 +0100

    cli: Typechecking for select queries

--- src/cli/lib/EasyBI/Cli/Command.hs
+++ src/cli/lib/EasyBI/Cli/Command.hs
@@ -1,23 +1,50 @@
 module EasyBI.Cli.Command
   ( Command (..)
   , TimestampColumn (..)
+  , SchemaConfig(..)
   , commandParser
   ) where
 
-import Options.Applicative (Parser, help, long, many, strOption)
+import           Options.Applicative (Parser, command, fullDesc, help, info,
+                                      long, many, progDesc, strOption,
+                                      subparser)
 
 newtype TimestampColumn = TimestampColumn{ unTimestampColumn :: String }
   deriving (Eq, Ord, Show)
 
-data Command =
-  CheckSchema
-    { sqlFile          :: FilePath -- ^ file with SQL create command
-    , timestampColumns :: [TimestampColumn] -- ^ Columns with timestamps (there is no TIMESTAMP type in Sqlite so we need to declare them manually)
+data SchemaConfig =
+  SchemaConfig
+    { scSqlFile          :: FilePath
+    , scTimestampColumns :: [TimestampColumn]
     }
+    deriving (Eq, Ord, Show)
+
+parseSchemaConfig :: Parser SchemaConfig
+parseSchemaConfig =
+  SchemaConfig
+    <$> strOption (long "sql-schema" <> help "File with SQL CREATE statement(s)")
+    <*> many (TimestampColumn <$> strOption (long "timestamp" <> help "Columns with timestamp values"))
+
+data Command =
+  CheckSchema SchemaConfig
+  | CheckTypes SchemaConfig FilePath
   deriving (Eq, Ord, Show)
 
 commandParser :: Parser Command
 commandParser =
+  subparser $
+    mconcat
+      [ command "check-schema" (info checkSchema (fullDesc <> progDesc "Read a file with SQL CREATE TABLE statements and check the schema"))
+      , command "check-types"  (info checkTypes  (fullDesc <> progDesc "Read a file with SQL SELECT statements and check their types"))
+      ]
+
+checkTypes :: Parser Command
+checkTypes =
+  CheckTypes
+    <$> parseSchemaConfig
+    <*> strOption (long "sql-query" <> help "File with SQL SELECT statement(s)")
+
+checkSchema :: Parser Command
+checkSchema =
   CheckSchema
-    <$> strOption (long "sql-schema" <> help "File with SQL CREATE statement(s)")
-    <*> many (TimestampColumn <$> strOption (long "timestamp" <> help "Columns with timestamp values"))
+    <$> parseSchemaConfig
