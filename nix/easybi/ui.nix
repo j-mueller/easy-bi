a1a134acf0bc9675361db3b68f835fece9c481f1
Jann Müller <j-mueller@users.noreply.github.com>
2023-10-26 16:58:07 +0200

    Use 'buildNpmPackage' to build UI

--- nix/easybi/ui.nix
+++ nix/easybi/ui.nix
@@ -1,10 +1,14 @@
-{ pkgs
-, dist
-, system
-}: derivation {
-    inherit system dist;
-    name = "easy-bi-ui";
-    coreutils = pkgs.coreutils;
-    builder = "${pkgs.bash}/bin/bash";
-    args = [./copy-ui.sh];
-}
+{ pkgs }:
+  pkgs.buildNpmPackage {
+    pname = "easy-bi-ui";
+    version = "1.0.0";
+    src = ../../src/ui;
+    # npmDepsHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    npmDepsHash = "sha256-IdWomP7N2ZTXFo2PwvsDkibdzWtZv5FDg+WZ6dR2cLA=";
+    npmBuildScript = "build";
+    # We just need the build output (from dist)
+    installPhase = ''
+      mkdir $out
+      cp -r dist/* $out
+    '';
+  }  
